ae3a4f9eab889cf47bf58e616f97df56ad025349
Yujie Xu <shenqing1225@gmail.com>
2022-09-18 12:48:29 -0700

    Adjust the paths

--- 3_replace_schedule_csv_path.R
+++ 3_replace_schedule_csv_path.R
@@ -1,5 +1,5 @@
 
-files = list.files("idf_add_sim_period_output", "*.idf")
+files = list.files("intermediate_data/idf_add_sim_period_output", "*.idf")
 
 head(files)
 
@@ -7,16 +7,15 @@ files = files[which(stringr::str_detect(files, "Family"))]
 
 for (f in files) {
   print(f)
-  lines = readLines(sprintf("idf_add_sim_period_output/%s", f))
+  lines = readLines(sprintf("intermediate_data/idf_add_sim_period_output/%s", f))
   newlines = sapply(lines, function(line) {
       if (stringr::str_detect(line, "/Users/sky/Sites/CBES/cbes_api/test/files/")) {
           newline = gsub("/Users/sky/Sites/CBES/cbes_api/test/files/",
-                         "/Users/yujiex/Dropbox/workLBNL/EESA/code/im3-wrf/res_schedule/", line, fixed = TRUE)
+                         "/Users/yujiex/Dropbox/workLBNL/EESA/meta_repo/xu_etal_2022_tbd/input_data/LA Prototypes/res_schedule/", line, fixed = TRUE)
       } else {
           newline = line
       }
       newline
   })
-  writeLines(newlines, sprintf("idf_add_sim_period_output/%s", f))
+  writeLines(newlines, sprintf("intermediate_data/idf_add_sim_period_output/%s", f))
 }
-
