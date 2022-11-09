1c8c91574c1e684e350addfb1a580dd0f1aa379f
Yujie Xu <shenqing1225@gmail.com>
2022-11-09 11:46:37 -0800

    Get summary stats

--- get_height_per_grid.R
+++ get_height_per_grid.R
@@ -33,8 +33,22 @@ df.building.nogeom %>%
 
 df.building <- readr::read_csv("output_data/building_metadata.csv")
 
-df.building %>%
+height.stats <- df.building %>%
+    dplyr::select(id.grid.finer, HEIGHT) %>%
+    na.omit() %>%
     dplyr::group_by(id.grid.finer) %>%
-    dplyr::summarise(HEIGHT = mean(HEIGHT)) %>%
+    dplyr::summarise_at(vars(HEIGHT), tibble::lst(min, median, mean, max, sd)) %>%
     dplyr::ungroup() %>%
-    readr::write_csv("intermediate_data/building_avg_height_finer_grid.csv")
+    {.}
+
+count.stats <- df.building %>%
+    dplyr::select(id.grid.finer, HEIGHT) %>%
+    dplyr::mutate(with.height = ifelse(is.na(HEIGHT), 0, 1)) %>%
+    dplyr::group_by(id.grid.finer) %>%
+    dplyr::summarise(n.building = n(), n.with.height = sum(with.height)) %>%
+    dplyr::ungroup() %>%
+    {.}
+
+height.stats %>%
+    dplyr::inner_join(count.stats, by = "id.grid.finer") %>%
+    readr::write_csv("intermediate_data/building_height_stats_finer_grid.csv")
