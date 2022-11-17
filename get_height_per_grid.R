76fa77fff0790d06167612adbe20a25d7dfe000b
Yujie Xu <shenqing1225@gmail.com>
2022-11-17 10:30:30 -0800

    get height summary at finer grid level in matrix form

--- get_height_per_grid.R
+++ get_height_per_grid.R
@@ -52,3 +52,52 @@ count.stats <- df.building %>%
 height.stats %>%
     dplyr::inner_join(count.stats, by = "id.grid.finer") %>%
     readr::write_csv("intermediate_data/building_height_stats_finer_grid.csv")
+
+
+mat <- tibble::tibble(wrf.grid.id = all.wrf.ids) %>%
+    dplyr::left_join(df.i, by=c("wrf.grid.id" = "id")) %>%
+    dplyr::select(wrf.grid.id, value) %>%
+    tidyr::replace_na(list(value="NaN")) %>%
+    .$value %>%
+    matrix(nrow=num.rows, byrow=TRUE) %>%
+    {.}
+
+get.all.wrf.ids <- function(grid.level) {
+    if (grid.level == "coarse") {
+        num.rows = 14
+        num.cols = 14
+        all.wrf.ids = 0:(num.rows * num.cols - 1)
+    } else if (grid.level == "finer"){
+        num.rows = 384
+        num.cols = 339
+        all.wrf.ids = 0:(num.rows * num.cols - 1)
+    }
+    all.wrf.ids
+}
+
+## rename the column to export in df to "value" first
+## rename grid id column to "id.grid"
+df.to.matrix <- function(grid.level, df) {
+    all.wrf.ids <- get.all.wrf.ids(grid.level)
+    mat <- tibble::tibble(wrf.grid.id = all.wrf.ids) %>%
+        dplyr::left_join(df,
+                         by=c("wrf.grid.id" = "id.grid")) %>%
+        dplyr::select(wrf.grid.id, value) %>%
+        tidyr::replace_na(list(value="NaN")) %>%
+        .$value %>%
+        matrix(nrow=num.rows, byrow=TRUE) %>%
+        {.}
+    mat
+}
+
+df.to.matrix("finer", height.stats %>%
+                             dplyr::rename(value = mean,
+                                           id.grid = id.grid.finer)) %>%
+    write.table("intermediate_data/mean_height.txt", row.names = FALSE,
+                col.names = FALSE, sep=",", quote=FALSE)
+
+df.to.matrix("finer", height.stats %>%
+                      dplyr::rename(value = sd,
+                                    id.grid = id.grid.finer)) %>%
+    write.table("intermediate_data/sd_height.txt", row.names = FALSE,
+                col.names = FALSE, sep=",", quote=FALSE)
